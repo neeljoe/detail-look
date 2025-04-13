5aa77afd74dfc64232e7a217b08715c5cb936f02
neeljoe <joe.neel@gmail.com>
2025-04-14 00:45:27 +0530

    css-first config

--- src/components/navbar/NavbarLogo.tsx
+++ src/components/navbar/NavbarLogo.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router"
 const NavbarLogo = () => {
   return (
     <Link to="/" className="flex items-center">
-        <span className="font-logo text-3xl">
+        <span className="font-logo text-3xl text-primary">
             Savannah
         </span>
     </Link>
