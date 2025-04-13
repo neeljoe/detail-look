21bdca470e7ee2e5d122d48410850d8e48547839
neeljoe <joe.neel@gmail.com>
2025-04-13 13:15:18 +0530

    error

--- src/components/NavbarMain.tsx
+++ src/components/NavbarMain.tsx
@@ -1,7 +1,16 @@
+import NavbarLogo from "./navbar/NavbarLogo"
 
 const NavbarMain = () => {
   return (
-    <div>NavbarMain</div>
+   <nav className="bg-white shadow-sm sticky top-0 z-50">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between h-16">
+        <div className="flex items-center">
+          <NavbarLogo />
+        </div>
+      </div>
+    </div>
+   </nav>
   )
 }
 
---
+++ src/components/navbar/NavbarLogo.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router"
+
+
+
+const NavbarLogo = () => {
+  return (
+    <Link to="/" className="flex items-center">
+        <span className="font-logo text-3xl">
+            Savannah
+        </span>
+    </Link>
+  )
+}
+
+export default NavbarLogo
